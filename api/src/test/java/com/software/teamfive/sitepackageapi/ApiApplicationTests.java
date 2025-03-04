c0900789da1538751ea32a7434abb0649bba4cf7
Noah Sternberg <noahcs2002@gmail.com>
2025-03-04 16:12:52 -0600

    PT-16 Create spring boot

---
+++ api/src/test/java/com/software/teamfive/sitepackageapi/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.teamfive.sitepackageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
