170f0d6c9f3f1e5a4b70130118bea51dedf2a5ce
Noah Sternberg <noahcs2002@gmail.com>
2025-03-04 16:21:03 -0600

    Fix issue with java code

--- api/src/test/java/com/software/teamfive/sitepackageapi/ApiApplicationTests.java
+++ api/src/test/java/com/software/teamfive/sitepackageapi/ApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.software.teamfive.sitepackageapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
